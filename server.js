30119ed35d07c0536451cd6a792411ec87197adf
Ainur Farkh <farkhutdinov1398@gmail.com>
2019-07-29 20:47:46 +0300

    GraphQL initialized

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const expressGraphQL = require('express-graphql');
 
 const { connectDB } = require('./config/db');
 
@@ -10,13 +11,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-connectDB();
-
-app.get('/', (req, res, next) => {
-    res.send('Hello world!');
-});
+app.use(
+    '/api',
+    expressGraphQL({
+        schema: {},
+        rootValue: {},
+        graphiql: true,
+    }),
+);
 
 const main = () => {
+    connectDB();
     app.listen(PORT, () => console.log(`Server up and running at http://localhost:${PORT}`));
 };
 
